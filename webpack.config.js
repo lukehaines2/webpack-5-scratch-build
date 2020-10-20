dc29ccec4af7d548c05267f16eae8525a6d705ad
Luke Haines <lukehaines2@gmail.com>
2020-10-20 23:34:03 +0100

    fix config for build

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(__dirname, './src/index.js')
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     title: 'webpack Boilerplate',
-  //     template: path.resolve(__dirname, './src/template.html'), // template file
-  //     filename: 'index.html', // output file
-  //   }),
-  // ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    })
+  ]
 }
