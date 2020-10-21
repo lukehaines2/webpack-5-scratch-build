6c4334bcd72b5faa693a436fb4867c13952c8637
Luke Haines <lukehaines2@gmail.com>
2020-10-21 15:29:48 +0100

    html added to config with correct filenames

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     title: 'webpack Boilerplate',
-  //     template: path.resolve(__dirname, './src/template.html'), // template file
-  //     filename: 'index.html', // output file
-  //   })
-  // ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    })
+  ]
 }
